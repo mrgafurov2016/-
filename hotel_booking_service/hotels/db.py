1e26117ffa0ba8296f57747ecfa9a42d73baf95b
mrgafurov2016 <mr.gafurov2016@yandex.ru>
2025-07-30 00:21:22 +0500

    Update db.py

--- hotel_booking_service/hotels/db.py
+++ hotel_booking_service/hotels/db.py
@@ -2,13 +2,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 
 def get_connection():
-    return psycopg2.connect(
-        dbname="hotel_db",
-        user="hotel_user",
-        password="hotel_password",
-        host="localhost",  # или 'db' если в Docker
-        port="5432"
-    )
+    return psycopg2.connect()
 
 def execute_query(query, params=None, fetch=False, many=False):
     with get_connection() as conn:
