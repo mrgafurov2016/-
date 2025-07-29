d5969e52a870fc3ec33e6bdc7b036bdb7e5122c3
mrgafurov2016 <mr.gafurov2016@yandex.ru>
2025-07-30 00:24:50 +0500

    Update views.py

--- hotel_booking_service/hotels/views.py
+++ hotel_booking_service/hotels/views.py
@@ -2,23 +2,22 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from .db import get_connection
+from .models import Room
 
 @csrf_exempt
 def create_room(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
-            conn = get_connection()
-            cur = conn.cursor()
-            cur.execute(
-                "INSERT INTO rooms (number, floor, capacity) VALUES (%s, %s, %s) RETURNING id;",
-                (data['number'], data['floor'], data['capacity'])
+
+            # Создание записи через ORM
+            room = Room.objects.create(
+                number=data['number'],
+                floor=data['floor'],
+                capacity=data['capacity']
             )
-            room_id = cur.fetchone()[0]
-            conn.commit()
-            cur.close()
-            conn.close()
-            return JsonResponse({'id': room_id, 'message': 'Room created'})
+
+            return JsonResponse({'id': room.id, 'message': 'Room created'})
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
     return JsonResponse({'error': 'Method not allowed'}, status=405)
